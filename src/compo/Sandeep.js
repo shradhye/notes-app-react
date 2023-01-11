106990fbcd860c70e1fcd2a652e4ca509573cb56
Sandeep Shradhye <shradhye@gmail.com>
2023-01-11 15:36:40 +0530

    added accordion component

--- src/compo/Sandeep.js
+++ src/compo/Sandeep.js
@@ -1,8 +1,37 @@
-function Sandeep(){
-    return(
-        <>
-            <h1>Sandeep Works</h1>
-        </>
-    )
+import styles from './Sandeep.css';
+import Accordion from 'react-bootstrap/Accordion';
+
+function Sandeep() {
+  return (
+    <section class='col-6 text-left m-auto'>
+    <Accordion defaultActiveKey={0}>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+</section>
+  );
 }
+
 export default Sandeep;
